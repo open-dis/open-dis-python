01bc85d2c56f1147d17e7e0b0e536b48459a4a3c
Leif Gruenwoldt <leifer@gmail.com>
2018-04-09 23:33:54 -0400

    Update project url

--- src/main/python/distributed_interactive_simulation/setup.py
+++ src/main/python/distributed_interactive_simulation/setup.py
@@ -1,7 +1,7 @@
 setup(name='distibuted_interactive_simulation',
       version='1.0',
       description='implementation of DIS, IEEE-1278.1',
-      url='http://open-dis.sourceforge.net',
+      url='https://github.com/open-dis/open-dis-python',
       author='DMcG',
       author_email='mcgredo@nps.edu',
       license='BSD',
