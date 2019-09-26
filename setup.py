a85dc4e2235dc110a5238896a3f855d00e100d8c
Leif Gruenwoldt <leifer@gmail.com>
2019-09-25 22:20:27 -0400

    Use author fullname in package descriptor

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(name='opendis',
       version='1.0',
-      author='DMcG',
+      author='Don McGregor',
       author_email='mcgredo@nps.edu',
       description='implementation of DIS, IEEE-1278.1',
       url='https://github.com/open-dis/open-dis-python',
