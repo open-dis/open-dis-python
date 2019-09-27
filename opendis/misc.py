731ba216f9150e9d61f5c0fc92ef3825e9ff76cc
Leif Gruenwoldt <leifer@gmail.com>
2019-09-26 22:43:38 -0400

    Move non spec code from dis7.py

---
+++ opendis/misc.py
@@ -0,0 +1,89 @@
+# Maybe remove these. Not really used anymore.
+
+
+
+class PduContainer( object ):
+    """Used for XML compatability. A container that holds PDUs"""
+
+    def __init__(self):
+        """ Initializer for PduContainer"""
+        self.numberOfPdus = 0
+        """ Number of PDUs in the container list"""
+        self.pdus = []
+        """ record sets"""
+
+    def serialize(self, outputStream):
+        """serialize the class """
+        outputStream.write_int( len(self.pdus));
+        for anObj in self.pdus:
+            anObj.serialize(outputStream)
+
+
+
+    def parse(self, inputStream):
+        """"Parse a message. This may recursively call embedded objects."""
+
+        self.numberOfPdus = inputStream.read_int();
+        for idx in range(0, self.numberOfPdus):
+            element = null()
+            element.parse(inputStream)
+            self.pdus.append(element)
+
+
+
+class PduStream( object ):
+    """Non-DIS class, used to describe streams of PDUS when logging data to a SQL database. This is not in the DIS standard but can be helpful when logging to a Hibernate sql database"""
+
+    def __init__(self):
+        """ Initializer for PduStream"""
+        self.description =  [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        """ Longish description of this PDU stream"""
+        self.name =  [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        """ short description of this PDU stream"""
+        self.startTime = 0
+        """ Start time of recording, in Unix time (seconds since epoch)"""
+        self.stopTime = 0
+        """ stop time of recording, in Unix time (seconds since epoch)"""
+        self.pduCount = 0
+        """ how many PDUs in this stream"""
+        self.pdusInStream = []
+        """ variable length list of PDUs"""
+
+    def serialize(self, outputStream):
+        """serialize the class """
+        for idx in range(0, 512):
+            outputStream.write_byte( self.description[ idx ] );
+
+        for idx in range(0, 256):
+            outputStream.write_byte( self.name[ idx ] );
+
+        outputStream.write_long(self.startTime);
+        outputStream.write_long(self.stopTime);
+        outputStream.write_unsigned_int( len(self.pdusInStream));
+        for anObj in self.pdusInStream:
+            anObj.serialize(outputStream)
+
+
+
+    def parse(self, inputStream):
+        """"Parse a message. This may recursively call embedded objects."""
+
+        self.description = [0]*512
+        for idx in range(0, 512):
+            val = inputStream.read_byte()
+
+            self.description[  idx  ] = val
+
+        self.name = [0]*256
+        for idx in range(0, 256):
+            val = inputStream.read_byte()
+
+            self.name[  idx  ] = val
+
+        self.startTime = inputStream.read_long();
+        self.stopTime = inputStream.read_long();
+        self.pduCount = inputStream.read_unsigned_int();
+        for idx in range(0, self.pduCount):
+            element = null()
+            element.parse(inputStream)
+            self.pdusInStream.append(element)
