1e2d80be4263a8b2442c80b58297d54da7c8bb6b
Leif Gruenwoldt <leifer@gmail.com>
2020-09-29 00:18:51 -0400

    Update testEntityStatePdu.py

--- tests/testEntityStatePdu.py
+++ tests/testEntityStatePdu.py
@@ -71,7 +71,7 @@ class TestEntityStatePdu(unittest.TestCase):
         # TODO self.assertEqual(???, pdu.deadReckoningParameters)
         
         # Entity Marking
-        self.assertEqual("26        ", bytes(pdu.marking.characters).decode("ascii"))
+        self.assertEqual("26", bytes(pdu.marking.characters).decode("ascii"))
 
 
 if __name__ == '__main__':
