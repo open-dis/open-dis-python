1a85279ec5aa07a9074d5875afb367f700e24fdd
Leif Gruenwoldt <leifer@gmail.com>
2020-09-28 22:13:31 -0400

    Fix comment syntax

--- tests/testEntityStatePdu.py
+++ tests/testEntityStatePdu.py
@@ -14,22 +14,22 @@ class TestEntityStatePdu(unittest.TestCase):
         self.assertEqual(7, pdu.exerciseID)
         self.assertEqual(1, pdu.pduType)
         self.assertEqual(1, pdu.protocolFamily)
-        //self.assertEqual(0, pdu.timestamp)
+        "self.assertEqual(0, pdu.timestamp)
         self.assertEqual(144, pdu.length)
         self.assertEqual(0, pdu.padding)
 
-        // Entity ID
+        " Entity ID
         self.assertEquals(42, espdu.entityID.site);
         self.assertEquals(4, espdu.entityID.application);
         self.assertEquals(26, espdu.entityID.entity);
 
-        // Force ID
+        " Force ID
         self.assertEquals(1, espdu.forceId);
 
-        // Articulation Parameters
+        " Articulation Parameters
         self.assertEquals(0, espdu.articulationParameters.size);
 
-        // Entity Type (aka DIS Enumeration)
+        " Entity Type (aka DIS Enumeration)
         self.assertEquals(1, espdu.entityType.entityKind);
         self.assertEquals(1, espdu.entityType.domain);
         self.assertEquals(39, espdu.entityType.country);
@@ -38,7 +38,7 @@ class TestEntityStatePdu(unittest.TestCase):
         self.assertEquals(1, espdu.entityType.spec);
         self.assertEquals(0, espdu.entityType.extra);
 
-        // Alternative Entity Type
+        " Alternative Entity Type
         self.assertEquals(1, espdu.alternativeEntityType.entityKind);
         self.assertEquals(1, espdu.alternativeEntityType.domain);
         self.assertEquals(39, espdu.alternativeEntityType.country);
@@ -47,30 +47,30 @@ class TestEntityStatePdu(unittest.TestCase):
         self.assertEquals(1, espdu.alternativeEntityType.spec);
         self.assertEquals(0, espdu.alternativeEntityType.extra);
 
-        // Entity Linear Velocity
+        " Entity Linear Velocity
         self.assertEquals(0, espdu.entityLinearVelocity.x, 0);
         self.assertEquals(0, espdu.entityLinearVelocity.y, 0);
         self.assertEquals(0, espdu.entityLinearVelocity.z, 0);
 
-        // Entity Location
+        " Entity Location
         self.assertEquals(4374082.80485589, espdu.entityLocation.x, 0.001);
         self.assertEquals(1667679.95730107, espdu.entityLocation.y, 0.001);
         self.assertEquals(4318284.36890269, espdu.entityLocation.z, 0.001);
 
-        // Entity Orientation
+        " Entity Orientation
         self.assertEquals(1.93505, espdu.entityOrientation.psi, 0.001);
         self.assertEquals(0, espdu.entityOrientation.theta, 0.001);
         self.assertEquals(-2.31924, espdu.entityOrientation.phi, 0.001);
 
-        // Entity Appearance
+        " Entity Appearance
         self.assertEquals(0, espdu.entityAppearance_paintScheme);
         self.assertEquals(0, espdu.entityAppearance_mobility);
         self.assertEquals(0, espdu.entityAppearance_firepower);
 
-        // Dead Reckoning Parameters
-        // TODO self.assertEquals(???, espdu.deadReckoningParameters);
+        " Dead Reckoning Parameters
+        " TODO self.assertEquals(???, espdu.deadReckoningParameters);
         
-        // Entity Marking
+        " Entity Marking
         self.assertEquals("26        ", espdu.marking.characters);
 
 if __name__ == '__main__':
