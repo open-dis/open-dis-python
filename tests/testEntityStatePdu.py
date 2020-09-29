f9be53c0f6e934bcadd2d18191c81661c56e48da
Leif Gruenwoldt <leifer@gmail.com>
2020-09-28 22:38:29 -0400

    fix typo

--- tests/testEntityStatePdu.py
+++ tests/testEntityStatePdu.py
@@ -19,59 +19,59 @@ class TestEntityStatePdu(unittest.TestCase):
         self.assertEqual(0, pdu.padding)
 
         # Entity ID
-        self.assertEqual(42, espdu.entityID.site)
-        self.assertEqual(4, espdu.entityID.application)
-        self.assertEqual(26, espdu.entityID.entity)
+        self.assertEqual(42, pdu.entityID.site)
+        self.assertEqual(4, pdu.entityID.application)
+        self.assertEqual(26, pdu.entityID.entity)
 
         # Force ID
-        self.assertEqual(1, espdu.forceId)
+        self.assertEqual(1, pdu.forceId)
 
         # Articulation Parameters
-        self.assertEqual(0, espdu.articulationParameters.size)
+        self.assertEqual(0, pdu.articulationParameters.size)
 
         # Entity Type (aka DIS Enumeration)
-        self.assertEqual(1, espdu.entityType.entityKind)
-        self.assertEqual(1, espdu.entityType.domain)
-        self.assertEqual(39, espdu.entityType.country)
-        self.assertEqual(7, espdu.entityType.category)
-        self.assertEqual(2, espdu.entityType.subcategory)
-        self.assertEqual(1, espdu.entityType.spec)
-        self.assertEqual(0, espdu.entityType.extra)
+        self.assertEqual(1, pdu.entityType.entityKind)
+        self.assertEqual(1, pdu.entityType.domain)
+        self.assertEqual(39, pdu.entityType.country)
+        self.assertEqual(7, pdu.entityType.category)
+        self.assertEqual(2, pdu.entityType.subcategory)
+        self.assertEqual(1, pdu.entityType.spec)
+        self.assertEqual(0, pdu.entityType.extra)
 
         # Alternative Entity Type
-        self.assertEqual(1, espdu.alternativeEntityType.entityKind)
-        self.assertEqual(1, espdu.alternativeEntityType.domain)
-        self.assertEqual(39, espdu.alternativeEntityType.country)
-        self.assertEqual(7, espdu.alternativeEntityType.category)
-        self.assertEqual(2, espdu.alternativeEntityType.subcategory)
-        self.assertEqual(1, espdu.alternativeEntityType.spec)
-        self.assertEqual(0, espdu.alternativeEntityType.extra)
+        self.assertEqual(1, pdu.alternativeEntityType.entityKind)
+        self.assertEqual(1, pdu.alternativeEntityType.domain)
+        self.assertEqual(39, pdu.alternativeEntityType.country)
+        self.assertEqual(7, pdu.alternativeEntityType.category)
+        self.assertEqual(2, pdu.alternativeEntityType.subcategory)
+        self.assertEqual(1, pdu.alternativeEntityType.spec)
+        self.assertEqual(0, pdu.alternativeEntityType.extra)
 
         # Entity Linear Velocity
-        self.assertEqual(0, espdu.entityLinearVelocity.x, 0)
-        self.assertEqual(0, espdu.entityLinearVelocity.y, 0)
-        self.assertEqual(0, espdu.entityLinearVelocity.z, 0)
+        self.assertEqual(0, pdu.entityLinearVelocity.x, 0)
+        self.assertEqual(0, pdu.entityLinearVelocity.y, 0)
+        self.assertEqual(0, pdu.entityLinearVelocity.z, 0)
 
         # Entity Location
-        self.assertEqual(4374082.80485589, espdu.entityLocation.x, 0.001)
-        self.assertEqual(1667679.95730107, espdu.entityLocation.y, 0.001)
-        self.assertEqual(4318284.36890269, espdu.entityLocation.z, 0.001)
+        self.assertEqual(4374082.80485589, pdu.entityLocation.x, 0.001)
+        self.assertEqual(1667679.95730107, pdu.entityLocation.y, 0.001)
+        self.assertEqual(4318284.36890269, pdu.entityLocation.z, 0.001)
 
         # Entity Orientation
-        self.assertEqual(1.93505, espdu.entityOrientation.psi, 0.001)
-        self.assertEqual(0, espdu.entityOrientation.theta, 0.001)
-        self.assertEqual(-2.31924, espdu.entityOrientation.phi, 0.001)
+        self.assertEqual(1.93505, pdu.entityOrientation.psi, 0.001)
+        self.assertEqual(0, pdu.entityOrientation.theta, 0.001)
+        self.assertEqual(-2.31924, pdu.entityOrientation.phi, 0.001)
 
         # Entity Appearance
-        self.assertEqual(0, espdu.entityAppearance_paintScheme)
-        self.assertEqual(0, espdu.entityAppearance_mobility)
-        self.assertEqual(0, espdu.entityAppearance_firepower)
+        self.assertEqual(0, pdu.entityAppearance_paintScheme)
+        self.assertEqual(0, pdu.entityAppearance_mobility)
+        self.assertEqual(0, pdu.entityAppearance_firepower)
 
         # Dead Reckoning Parameters
-        # TODO self.assertEqual(???, espdu.deadReckoningParameters)
+        # TODO self.assertEqual(???, pdu.deadReckoningParameters)
         
         # Entity Marking
-        self.assertEqual("26        ", espdu.marking.characters)
+        self.assertEqual("26        ", pdu.marking.characters)
 
 if __name__ == '__main__':
     unittest.main()
