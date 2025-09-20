326415185fd4c6b29623593630155c528873b5cf
JS Ng <ngjunsiang@gmail.com>
2025-09-20 12:29:24 +0000

    feat: add convenience aliases

--- opendis/DataOutputStream.py
+++ opendis/DataOutputStream.py
@@ -7,6 +7,22 @@ This uses big endian (network) format
 from io import BufferedIOBase
 import struct
 
+from .types import (
+    int8,
+    int16,
+    int32,
+    int64,
+    uint8,
+    uint16,
+    uint32,
+    uint64,
+    float32,
+    float64,
+    char8,
+    char16,
+)
+
+
 class DataOutputStream:
     def __init__(self, stream: BufferedIOBase):
         self.stream = stream
@@ -48,3 +64,40 @@ class DataOutputStream:
     def write_unsigned_int(self, val: int) -> None:
         self.stream.write(struct.pack('>I', val))
 
+    def write_bytes(self, val: bytes) -> None:
+        """Write bytes to the stream."""
+        self.stream.write(val)
+
+    # Aliases for convenience
+    def write_char8(self, val: char8) -> None:
+        self.write_byte(ord(val))
+
+    def write_float32(self, val: float32) -> None:
+        self.write_float(val)
+
+    def write_float64(self, val: float64) -> None:
+        self.write_double(val)
+
+    def write_int8(self, val: int8) -> None:
+        self.write_byte(val)
+
+    def write_int16(self, val: int16) -> None:
+        self.write_short(val)
+
+    def write_int32(self, val: int32) -> None:
+        self.write_int(val)
+
+    def write_int64(self, val: int64) -> None:
+        self.write_long(val)
+
+    def write_uint8(self, val: uint8) -> None:
+        self.write_unsigned_byte(val)
+
+    def write_uint16(self, val: uint16) -> None:
+        self.write_unsigned_short(val)
+
+    def write_uint32(self, val: uint32) -> None:
+        self.write_unsigned_int(val)
+
+    def write_uint64(self, val: uint64) -> None:
+        self.stream.write(struct.pack('>Q', val))
