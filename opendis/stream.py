091c88b03e986f4704cff1090ed8e2781267c24f
JS Ng <ngjunsiang@gmail.com>
2025-09-20 12:33:46 +0000

    refactor: centralize DataInputStream and DataOutputStream under the `stream` namespace

---
+++ opendis/stream.py
@@ -0,0 +1,9 @@
+"""Stream utilities
+
+This module provides utilities for handling streams in the OpenDIS library.
+"""
+
+__all__ = ['DataInputStream', 'DataOutputStream']
+
+from .DataInputStream import DataInputStream
+from .DataOutputStream import DataOutputStream
